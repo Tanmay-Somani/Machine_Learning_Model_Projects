c6414a3da3323100349292d17c52db60b6d4e147
Tanmay Somani <tanmaysomani2003@gmail.com>
2025-06-10 19:48:38 +0530

    basic dummy data

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -1,4 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-x
+
+X = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
+y = np.array([2, 4, 5, 4, 5])
+
+model = LinearRegression()
+model.fit(X, y)
+
+y_pred = model.predict(X)
+
+plt.scatter(X, y)
+plt.plot(X, y_pred, color='red')
+plt.title("Simple Linear Regression")
+plt.xlabel("X")
+plt.ylabel("y")
+plt.show()
